563052d70bffd2fba5cff3a12e897aad5ac62322
muratayusuke <info@muratayusuke.com>
2015-07-29 18:14:39 +0900

    bugfix

--- lib/itamae/plugin/recipe/phantomjs/source.rb
+++ lib/itamae/plugin/recipe/phantomjs/source.rb
@@ -7,9 +7,22 @@ end
 execute 'apt-get update'
 
 %w(build-essential g++ flex bison gperf ruby perl libsqlite3-dev
-  libfontconfig1-dev libicu-dev libfreetype6 libssl-dev libpng-dev
-  libjpeg-dev python libx11-dev libxext-dev ttf-mscorefonts-installer git).each do |p|
+  libfontconfig1-dev libicu-dev libfreetype6 libssl-dev libjpeg-dev
+  python libx11-dev libxext-dev ttf-mscorefonts-installer git).each do |p|
   package p
+end
+
+execute 'install libpng' do
+  cwd '/tmp'
+  command <<-EOC
+    wget ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/libpng-1.6.18.tar.gz
+    tar xvf libpng-1.6.18.tar.gz
+    cd libpng-1.6.18
+    ./configure
+    make
+    make install
+  EOC
+  not_if 'test -e /usr/local/lib/libpng16.so'
 end
 
 tmp_dir = '/tmp/phantomjs'
@@ -18,7 +31,7 @@ execute "git clone git://github.com/ariya/phantomjs.git #{tmp_dir}" do
   not_if "test -d #{tmp_dir}"
 end
 
-execute 'git fetch' do
+execute 'git fetch && git clean -xfd' do
   cwd tmp_dir
 end
 

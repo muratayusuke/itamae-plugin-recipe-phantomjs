6cbb675ca73a6367f83a0772f7122b656a69b131
muratayusuke <info@muratayusuke.com>
2015-07-29 20:37:59 +0900

    add binary recipe

---
+++ lib/itamae/plugin/recipe/phantomjs/binary.rb
@@ -0,0 +1,3 @@
+package 'libicu52'
+execute 'curl --output /usr/local/bin/phantomjs https://s3.amazonaws.com/circle-support-bucket/phantomjs/phantomjs-2.0.1-linux-x86_64-dynamic'
+execute 'chmod a+x /usr/local/bin/phantomjs'
